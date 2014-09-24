32f1fc9300579ebc0921b798042c6e701089b405
Joe Yennaco <joe@joeyennaco.com>
2014-09-23 22:51:52 -0400

    started work on automated vbox install on debian-based linux

---
+++ virtualbox/install-vbox-debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Downloading VirtualBox 4.3.13 ..."
+wget http://download.virtualbox.org/virtualbox/4.3.14/virtualbox-4.3_4.3.14-95030~Ubuntu~raring_amd64.deb
+
+echo "Installing VirtualBox ..."
+
